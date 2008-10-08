9f3bcd631aa3f43b7b23d92303dc5f6f7899ff3e
chris.ruffalo <chris.ruffalo@859a9a3a-9550-11dd-afab-83b092e25ceb>
2008-10-08 23:11:14 +0000

    four more test cases (two for client, two for server) to test boundary conditions in @Length annotation (LengthValidator.java) and to prove that I follow my test plan in the wiki.

--- test/com/google/gwt/validation/server/LengthServerTest.java
+++ test/com/google/gwt/validation/server/LengthServerTest.java
@@ -31,7 +31,7 @@ import com.google.gwt.validation.client.Length;
 import com.google.gwt.validation.client.LengthValidator;
 
 public class LengthServerTest {
-	
+
 	private Length l = new Length(){
 
 		public String[] groups() {
@@ -53,47 +53,73 @@ public class LengthServerTest {
 		public Class<? extends Annotation> annotationType() {
 			return null;
 		}
-		
+
 	};
-	
+
 	@Test
 	public void testUnderLength() {
-		
+
 		//create length constraint
 		LengthValidator lv = new LengthValidator();
 		lv.initialize(this.l);
-		
+
 		//create test string
 		String testString =  "1234";
-		
+
 		//assert false on validation
 		assertFalse("Validation of test string (length 4) should be under minimum and return false.", lv.isValid(testString));
 	}
-	
+
 	@Test
 	public void testWithinLength() {
 		//create length constraint
 		LengthValidator lv = new LengthValidator();
 		lv.initialize(this.l);
-		
+
 		//create test string
 		String testString =  "123456";
-		
+
 		//assert false on validation
 		assertTrue("Validation of test string (length 6) should be within limits return true.", lv.isValid(testString));
 	}
-	
+
 	@Test
 	public void testOverLength() {
 		//create length constraint
 		LengthValidator lv = new LengthValidator();
 		lv.initialize(this.l);
-		
+
 		//create test string
 		String testString =  "1234567891011";
-		
+
 		//assert false on validation
 		assertFalse("Validation of test string (length 13) should be over maximum and return false.", lv.isValid(testString));
 	}
-	
+
+	@Test
+	public void testAtMinimumLength() {
+		//create length constraint
+		LengthValidator lv = new LengthValidator();
+		lv.initialize(this.l);
+
+		//create test string
+		String testString =  "12345";
+
+		//assert false on validation
+		assertTrue("Validation of test string (length 5) should be at minimum and return true.", lv.isValid(testString));
+	}
+
+	@Test
+	public void testAtMaximumLength() {
+		//create length constraint
+		LengthValidator lv = new LengthValidator();
+		lv.initialize(this.l);
+
+		//create test string
+		String testString =  "1234567890";
+
+		//assert false on validation
+		assertTrue("Validation of test string (length 10) should be at maximum and return true.", lv.isValid(testString));
+	}
+
 }
