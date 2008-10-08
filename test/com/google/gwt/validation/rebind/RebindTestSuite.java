0f0f96f2f497222fb8e23c02018c6ffa321cb49c
chris.ruffalo <chris.ruffalo@859a9a3a-9550-11dd-afab-83b092e25ceb>
2008-10-08 16:41:14 +0000

    added gplv2 notice to all files added more intermediary folders and files to ignore list

--- test/com/google/gwt/validation/rebind/RebindTestSuite.java
+++ test/com/google/gwt/validation/rebind/RebindTestSuite.java
@@ -1,5 +1,25 @@
 package com.google.gwt.validation.rebind;
 
+/*
+GWT-Validation Framework - Annotation based validation for the GWT Framework
+
+Copyright (C) 2008  Christopher Ruffalo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
