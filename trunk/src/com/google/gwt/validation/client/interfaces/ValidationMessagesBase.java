3c05873e8a0c969720bf850fcbccf46a2d8455d6
ladislav.gazo <ladislav.gazo@859a9a3a-9550-11dd-afab-83b092e25ceb>
2010-03-10 15:09:16 +0000

    validation messages for all constraints are in place

--- trunk/src/com/google/gwt/validation/client/interfaces/ValidationMessagesBase.java
+++ trunk/src/com/google/gwt/validation/client/interfaces/ValidationMessagesBase.java
@@ -11,14 +11,24 @@ import com.google.gwt.i18n.client.Constants;
  * @author ladislav.gazo
  */
 public interface ValidationMessagesBase extends Constants {
-	String javax_validation_constraints_NotNull_message();
-	String javax_validation_constraints_Size_message();
+		
+	String javax_validation_constraints_AssertFalse_message();
+	String javax_validation_constraints_AssertTrue_message();
+	String javax_validation_constraints_DecimalMax_message();
+	String javax_validation_constraints_DecimalMin_message();
+	String javax_validation_constraints_Digits_message();
+	String javax_validation_constraints_Future_message();
 	String javax_validation_constraints_Min_message();
 	String javax_validation_constraints_Max_message();
+	String javax_validation_constraints_NotNull_message();
+	String javax_validation_constraints_Null_message();
+	String javax_validation_constraints_Past_message();
+	String javax_validation_constraints_Pattern_message();
+	String javax_validation_constraints_Size_message();
 	
-	String constraint_notNull();
-	String constraint_size();
-	String constraint_min();
-	String constraint_max();
-	String constraint_pattern();
+//	String constraint_notNull();
+//	String constraint_size();
+//	String constraint_min();
+//	String constraint_max();
+//	String constraint_pattern();
 }
