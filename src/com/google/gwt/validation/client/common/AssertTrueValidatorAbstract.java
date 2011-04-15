45da8374eb4c7aff050a8a9183d70fc0fe50cb27
david.renoux <david.renoux@gmail.com>
2011-04-15 14:15:30 +0000

    Fixed issue 30 For details please visit: http://code.google.com/p/gwt-validation/wiki/GWT_220_compatibility

--- src/com/google/gwt/validation/client/common/AssertTrueValidatorAbstract.java
+++ src/com/google/gwt/validation/client/common/AssertTrueValidatorAbstract.java
@@ -1,20 +1,20 @@
 package com.google.gwt.validation.client.common;
 
 /*
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 import java.util.Map;
 
@@ -24,24 +24,25 @@ import java.util.Map;
  * @author chris
  *
  */
-public abstract class AssertTrueValidatorAbstract{
+public abstract class AssertTrueValidatorAbstract
+{
 
+    public boolean isValid(final Object value)
+    {
+        if (value == null)
+            return true;
 
-    public boolean isValid(Object value) {
-        if (value == null) return true;
-        
         boolean isvalid = false;
-        
-        try {
-        	isvalid = (Boolean)value;
-        } catch (Exception ex) {
-        	//isn't boolean / can't be casted as such
-        }
-        
+
+        if (value instanceof Boolean)
+            isvalid = (Boolean) value;
+
         return isvalid;
     }
-	public void initialize(Map<String, String> propertyMap) {
-		
-	}
+
+    public void initialize(final Map<String, String> propertyMap)
+    {
+
+    }
 
 }
--- src/com/google/gwt/validation/client/common/FutureValidatorAbstract.java
+++ src/com/google/gwt/validation/client/common/FutureValidatorAbstract.java
@@ -3,24 +3,26 @@ package com.google.gwt.validation.client.common;
 import java.util.Date;
 import java.util.Map;
 
-public abstract class FutureValidatorAbstract {
-	public boolean isValid(Object value) {
-		if (value == null)
-			return true;
+public abstract class FutureValidatorAbstract
+{
+    public boolean isValid(final Object value)
+    {
+        if (value == null)
+            return true;
 
-		boolean isvalid = false;
+        boolean isvalid = false;
 
-		try {
-			Date date = (Date) value;
-			isvalid = date.after(new Date());
-		} catch (Exception ex) {
+        if (value instanceof Date)
+        {
+            Date date = (Date) value;
+            isvalid = date.after(new Date());
+        }
 
-		}
+        return isvalid;
+    }
 
-		return isvalid;
-	}
+    public void initialize(final Map<String, String> propertyMap)
+    {
 
-	public void initialize(Map<String, String> propertyMap) {
-
-	}
+    }
 }
