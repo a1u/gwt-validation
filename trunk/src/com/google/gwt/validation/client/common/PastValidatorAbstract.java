66cf209b474a2f04ef40f0082eef3e5dd6e5414a
david.renoux@gmail.com <david.renoux@gmail.com@859a9a3a-9550-11dd-afab-83b092e25ceb>
2011-04-15 14:15:30 +0000

    Fixed issue 30 For details please visit: http://code.google.com/p/gwt-validation/wiki/GWT_220_compatibility

--- trunk/src/com/google/gwt/validation/client/common/PastValidatorAbstract.java
+++ trunk/src/com/google/gwt/validation/client/common/PastValidatorAbstract.java
@@ -3,26 +3,28 @@ package com.google.gwt.validation.client.common;
 import java.util.Date;
 import java.util.Map;
 
-public abstract class PastValidatorAbstract {
+public abstract class PastValidatorAbstract
+{
 
-	public boolean isValid(Object value) {
-		if (value == null)
-			return true;
+    public boolean isValid(final Object value)
+    {
+        if (value == null)
+            return true;
 
-		boolean isvalid = false;
+        boolean isvalid = false;
 
-		try {
-			Date date = (Date) value;
-			isvalid = date.before(new Date());
-		} catch (Exception ex) {
+        if (value instanceof Date)
+        {
+            Date date = (Date) value;
+            isvalid = date.before(new Date());
+        }
 
-		}
+        return isvalid;
+    }
 
-		return isvalid;
-	}
+    public void initialize(final Map<String, String> propertyMap)
+    {
 
-	public void initialize(Map<String, String> propertyMap) {
-
-	}
+    }
 
 }
--- trunk/src/com/google/gwt/validation/client/common/SizeValidatorAbstract.java
+++ trunk/src/com/google/gwt/validation/client/common/SizeValidatorAbstract.java
@@ -3,140 +3,79 @@ package com.google.gwt.validation.client.common;
 import java.util.Collection;
 import java.util.Map;
 
+public abstract class SizeValidatorAbstract
+{
 
-public abstract class SizeValidatorAbstract {
+    protected int minimum;
+    protected int maximum;
 
-	protected int minimum;
-	protected int maximum;
+    public void initialize(final Map<String, String> propertyMap)
+    {
 
-	public void initialize(Map<String, String> propertyMap) {
+        /*
+         * !!!! Notice that these keys are exactly the same as the method names on the annotation !!!!
+         */
 
-		/*
-		 * !!!! Notice that these keys are exactly the same as the method names
-		 * on the annotation !!!!
-		 */
+        this.minimum = Integer.parseInt(getMin(propertyMap));
+        this.maximum = Integer.parseInt(getMax(propertyMap));
 
-		this.minimum = Integer.parseInt(getMin(propertyMap));
-		this.maximum = Integer.parseInt(getMax(propertyMap));
+    }
 
-	}
+    private String getMin(final Map<String, String> propertyMap)
+    {
+        String m = propertyMap.get("minimum");
+        if (m == null)
+            m = propertyMap.get("min");
+        return m;
+    }
 
-	private String getMin(Map<String, String> propertyMap) {
-		String m = propertyMap.get("minimum");
-		if (m == null) {
-			m = propertyMap.get("min");
-		}
-		return m;
-	}
+    private String getMax(final Map<String, String> propertyMap)
+    {
+        String m = propertyMap.get("maximum");
+        if (m == null)
+            m = propertyMap.get("max");
+        return m;
+    }
 
-	private String getMax(Map<String, String> propertyMap) {
-		String m = propertyMap.get("maximum");
-		if (m == null) {
-			m = propertyMap.get("max");
-		}
-		return m;
-	}
+    public boolean isValid(final Object value)
+    {
+        if (value == null)
+            return true;
 
-	public boolean isValid(Object value) {
-		if (value == null)
-			return true;
+        boolean valid = false;
 
-		boolean valid = false;
+        int size = -1;
 
-		int size = -1;
+        while (size < 0)
+        {
+            if (value instanceof Object[])
+                size = ((Object[]) value).length;
+            else if (value instanceof int[])
+                size = ((int[]) value).length;
+            else if (value instanceof float[])
+                size = ((float[]) value).length;
+            else if (value instanceof double[])
+                size = ((double[]) value).length;
+            else if (value instanceof long[])
+                size = ((long[]) value).length;
+            else if (value instanceof Collection<?>)
+                size = ((Collection<?>) value).size();
+            else if (value instanceof Map<?, ?>)
+                size = ((Map<?, ?>) value).size();
+            else if (value instanceof String)
+                size = ((String) value).trim().length();
+            break;
+        }
 
-		if (size < 0) {
+        // no size found
+        if (size == -1)
+            return true;
 
-			try {
-				size = ((Object[]) value).length;
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-			}
+        // else compute value
+        if (size >= 0 && this.maximum >= size && this.minimum <= size)
+            valid = true;
 
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((int[]) value).length;
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((float[]) value).length;
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((double[]) value).length;
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((long[]) value).length;
-			} catch (Exception ex) {
-				// ex.printStackTrace();
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((Collection<?>) value).size();
-			} catch (Exception ex) {
-
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((Map<?, ?>) value).size();
-			} catch (Exception ex) {
-
-			}
-
-		}
-
-		if (size < 0) {
-
-			try {
-				size = ((String) value).trim().length();
-			} catch (Exception ex) {
-
-			}
-
-			// if(size >= 0 && this.maximum >= size && this.minimum <= size)
-			// valid = true;
-
-		}
-
-		// no size found
-		if (size == -1)
-			return true;
-
-		// else compute value
-		if (size >= 0 && this.maximum >= size && this.minimum <= size)
-			valid = true;
-
-		return valid;
-	}
+        return valid;
+    }
 
 }
